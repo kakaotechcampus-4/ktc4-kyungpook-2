34b326f8cff28747d5c3a47c83268b8ef86b9248
hyojeong <hyojeong6208@gmail.com>
2026-09-14 18:31:15 +0900

    chore: Next.js frontend 제거, frontend-vite를 frontend로 이동

--- frontend/src/app/(org)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { OrgShell } from "@/components/org/Shell";
-
-export default function OrgLayout({ children }: { children: React.ReactNode }) {
-  return <OrgShell>{children}</OrgShell>;
-}
--- frontend/src/app/parent/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ParentShell } from "@/components/parent/Shell";
-
-export default function ParentLayout({ children }: { children: React.ReactNode }) {
-  return <ParentShell>{children}</ParentShell>;
-}
